eec5101965740e2711472225a3d776651824f17e
knowledgevyasa@gmail.com <knowledgevyasa@gmail.com>
2020-05-01 23:49:18 +0530

    prod changes

--- src/app/examples/concordhome/concordhome.component.ts
+++ src/app/examples/concordhome/concordhome.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer } from '@angular/core';
+import { NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-concordhome',
@@ -7,8 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConcordhomeComponent implements OnInit {
 
-  constructor() { }
+  page = 4;
+    page1 = 5;
+    focus;
+    focus1;
+    focus2;
+    date: {year: number, month: number};
+    model: NgbDateStruct;
+    constructor( private renderer : Renderer) {}
+    isWeekend(date: NgbDateStruct) {
+        const d = new Date(date.year, date.month - 1, date.day);
+        return d.getDay() === 0 || d.getDay() === 6;
+    }
 
+    isDisabled(date: NgbDateStruct, current: {month: number}) {
+        return date.month !== current.month;
+    }
   ngOnInit() {
   }
 
